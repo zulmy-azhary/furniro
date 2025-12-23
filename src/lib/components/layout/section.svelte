e5c2ecaa3891ba995c3fbeccf0192c00ba98dd60
Zulmy Azhary <zulmyazhary32@gmail.com>
2025-12-23 11:37:55 +0800

    feat: update section component

--- src/lib/components/layout/section.svelte
+++ src/lib/components/layout/section.svelte
@@ -2,13 +2,15 @@
 	import { cn } from '@/utils';
 	import type { HTMLAttributes } from 'svelte/elements';
 
-	type SectionProps = HTMLAttributes<HTMLElement>;
+	type SectionProps = HTMLAttributes<HTMLElement> & {
+		wrapperClassName?: SectionProps['class'],
+	};
 
 	let { class: className, children, ...props }: SectionProps = $props();
 </script>
 
-<section class={cn("px-4 2xl:px-24 flex justify-center w-full", className)} {...props}>
-	<div class="container">
+<section class={cn("px-4 2xl:px-24 flex justify-center w-full", props.wrapperClassName)} {...props}>
+	<div class={cn("container", className)}>
 		{@render children?.()}
 	</div>
 </section>
