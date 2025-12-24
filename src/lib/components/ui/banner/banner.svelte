bbdfceff5edcd83ab7b68995f3b2779501ca280c
Zulmy Azhary <zulmyazhary32@gmail.com>
2025-12-24 14:32:24 +0800

    feat: create banner component

---
+++ src/lib/components/ui/banner/banner.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import logo from '@/assets/logo.svg';
+	import banner from '@/assets/images/banner.png';
+	import { Breadcrumb, Item, Link, List, Separator } from '@/components/ui/breadcrumb';
+
+	type BannerProps = {
+		image?: string;
+		title: {
+			name: string;
+			path: string;
+		};
+	};
+
+	let { image, title }: BannerProps = $props();
+</script>
+
+<section class="relative py-24 w-full -mx-4 2xl:-mx-24 flex flex-col items-center gap-y-2">
+	<div class="absolute inset-0 -z-10">
+		<img src={image ?? banner} alt="Banner" class="object-cover size-full opacity-50" />
+	</div>
+	<img src={logo} alt="Furniro's Logo" />
+	<h2 class="text-5xl font-medium">{title.name}</h2>
+	<Breadcrumb>
+		<List>
+			<Item>
+				<Link href="/" class="font-medium">Home</Link>
+			</Item>
+			<Separator />
+			<Item>
+				<Link href={title.path}>{title.name}</Link>
+			</Item>
+		</List>
+	</Breadcrumb>
+</section>
