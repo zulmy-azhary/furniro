e3f1604e676a118d0bf267e9f7b9a8b7f7041018
Zulmy Azhary <zulmyazhary32@gmail.com>
2025-12-24 14:36:13 +0800

    feat(contact): create contact page

---
+++ src/routes/(app)/contact/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import MetaTitle from '@/components/misc/meta-title.svelte';
+	import { Banner } from '@/components/ui/banner';
+</script>
+
+<MetaTitle title="Contact" />
+<Banner title={{ name: 'Contact', path: '/contact' }} />
