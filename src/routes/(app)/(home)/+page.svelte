b62f30053e4a211f352630e7d540fa578c088418
Zulmy Azhary <zulmyazhary32@gmail.com>
2025-12-24 09:42:14 +0800

    feat: move home page to grouped route, import all home section's components

---
+++ src/routes/(app)/(home)/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import MetaTitle from '@/components/misc/meta-title.svelte';
+	import {
+		CategorySection,
+		HeroSection,
+		InspirationSection,
+		ProductSection,
+		ShareSection
+	} from './_components';
+</script>
+
+<MetaTitle title="Home" />
+
+<HeroSection />
+<CategorySection />
+<ProductSection />
+<InspirationSection />
+<ShareSection />
