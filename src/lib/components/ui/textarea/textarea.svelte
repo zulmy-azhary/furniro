98560755a0e64a558dbf33d396ec151ef9ba910d
Zulmy Azhary <zulmyazhary32@gmail.com>
2025-12-25 16:20:53 +0800

    style: update input & textarea's base style component

--- src/lib/components/ui/textarea/textarea.svelte
+++ src/lib/components/ui/textarea/textarea.svelte
@@ -15,7 +15,7 @@
 	bind:this={ref}
 	data-slot={dataSlot}
 	class={cn(
-		"border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+		"border-slate-400 placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-slate-400/30 flex field-sizing-content min-h-40 w-full rounded-lg border bg-transparent px-8 py-6 text-sm transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-base",
 		className
 	)}
 	bind:value
